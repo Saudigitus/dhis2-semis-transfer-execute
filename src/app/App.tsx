9583d95213d3da4daf74dd82b129e8375848eece
cnandza <cnandza@saudigitus.org>
2025-05-30 11:37:24 +0200

    wrappers removed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,22 +2,11 @@ import '../index.css'
 import React from 'react'
 import './App.module.css'
 import { Router } from '../components/routes'
-import { HashRouter } from 'react-router-dom'
-import { useConfig } from '@dhis2/app-runtime'
-import { AppWrapper } from 'dhis2-semis-components'
 
 const TransferExecute = () => {
-    const { baseUrl } = useConfig()
 
     return (
-        <AppWrapper
-            baseUrl={baseUrl}
-            dataStoreKey='dataStore/semis/values'
-        >
-            <HashRouter>
-                <Router />
-            </HashRouter>
-        </AppWrapper>
+        <Router />
     )
 }
 
