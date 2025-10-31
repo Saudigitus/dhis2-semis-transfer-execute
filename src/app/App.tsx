42385e8a25cf8007a414d204d3ce78dc4d1352bd
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2025-10-31 11:16:40 +0200

    feat(i18n): add internationalization support for transfer execute module
    - Add i18n translations for English, French and Portuguese
    - Update components to use i18n for text rendering
    - Remove hardcoded strings and replace with translation keys
    - Add locales directory with translation files
    - Update package dependencies for i18n support

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,8 +5,9 @@ import { Router } from '../components/routes'
 import { AppWrapper } from 'dhis2-semis-components'
 import { useConfig } from '@dhis2/app-runtime'
 import { HashRouter } from 'react-router-dom'
+import { D2I18n } from 'dhis2-semis-types'
 
-const TransferExecute = () => {
+const TransferExecute = ({ i18n }: { i18n: D2I18n }) => {
     const { baseUrl } = useConfig()
 
     return (
@@ -16,7 +17,7 @@ const TransferExecute = () => {
         //     schoolCalendarKey={"dataStore/semis/schoolCalendar"}
         // >
         //     <HashRouter>
-        <Router />
+        <Router i18n={i18n} />
         //     </HashRouter>
         // </AppWrapper>
     )
--- src/pages/info/infoPage.tsx
+++ src/pages/info/infoPage.tsx
@@ -1,15 +1,16 @@
 import { InfoPage } from "dhis2-semis-components";
+import { D2I18n } from "dhis2-semis-types";
 
-export default function CustomInfoPage() {
+export default function CustomInfoPage({ i18n }: { i18n: D2I18n }) {
     return (
         <InfoPage
-            title="SEMIS-Transfer-Execute"
+            title={i18n.t("SEMIS-Transfer-Execute")}
             sections={[
                 {
-                    sectionTitle: "Follow the instructions to proceed:",
+                    sectionTitle: i18n.t("Follow the instructions to proceed"),
                     instructions: [
-                        "Select the Organization unit you want to view data",
-                        "Use global filters(Class, Grade and Academic Year)",
+                        i18n.t("Select the Organization unit you want to view data"),
+                        i18n.t("Use global filters(Class, Grade and Academic Year)")
                     ],
                 },
             ]}
